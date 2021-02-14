0ed9997792431a145a822f6eb4bdce68a4391c6d
Denis <lopatinskiyden@gmail.com>
2021-02-14 19:11:06 +0200

    Add request on "cors-anywhere"

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ const App = () => {
 
   let [mapCards, setMapCards] = React.useState<any>();
   React.useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=London&appid=14358bcdca1ca0a20ba95b2d02b61bd4")
+    fetch("https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?q=London&appid=14358bcdca1ca0a20ba95b2d02b61bd4", {
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    })
       .then(rest => rest.json())
       .then(json => {
 
@@ -23,7 +27,11 @@ const App = () => {
   const city: any = React.createRef();
   const req = () => {
     if (/^[A-zА-я]+/.test(inp.current.value)) {
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inp.current.value}&appid=14358bcdca1ca0a20ba95b2d02b61bd4`)
+      fetch(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?q=${inp.current.value}&appid=14358bcdca1ca0a20ba95b2d02b61bd4`, {
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        }
+      })
         .then(rest => rest.json())
         .then(json => {
 
